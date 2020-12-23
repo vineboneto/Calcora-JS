15f8284975da5590124c565a36978fa359d168e4
vineboneto <vineboneto@gmail.com>
2020-12-23 00:36:53 -0300

    update layout

--- src/js/script.js
+++ src/js/script.js
@@ -25,8 +25,12 @@ function hourMinutesMaks (input)  {
     input.addEventListener('keyup', (event) => {
         event.currentTarget.maxLength = 5
         let value = event.currentTarget.value
-        value = value.replace(/\D/g)
-        value = value.replace(/(\d{2})(\d{2})/, "$1:$2")
+        if (value.length > 4) {
+            value = ''
+        } else {
+            value = value.replace(/\D/g)
+            value = value.replace(/(\d{2})(\d{2})/, "$1:$2")
+        }
         event.currentTarget.value = value
         settingTime()
     })
